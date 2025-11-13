b4d6ce99a63fc7bb6c5444dc4965bdddb8e19fb7
AngelDubier <153401649+AngelDubier@users.noreply.github.com>
2025-11-13 15:25:56 -0500

    Refactor index.tsx to export App component
    Removed ReactDOM rendering logic and exported App component.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,4 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+export default function App() {
+  return <div style={{ padding: 16 }}>Hola 👋 App cargó bien.</div>;
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
